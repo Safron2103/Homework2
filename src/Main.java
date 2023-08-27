6d7e354bd2cd1b3fe39cf11c7f806929636c3ff0
Safron2103 <Safron2103@mail.ru>
2023-08-27 05:43:34 +0300

    Homework2 for checking

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+    public static void main(String[] args) {
+
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var boxerWeitght1 = 78.2;
+        var boxerWeitght2 = 82.7;
+        var totalWeitght = boxerWeitght1 + boxerWeitght2;
+        System.out.println(totalWeitght);
+        var massDifference = boxerWeitght2 - boxerWeitght1;
+        System.out.println(massDifference);
+
+        var overload = (boxerWeitght2 / boxerWeitght1) % totalWeitght;
+        System.out.println(overload);
+
+        var totalHours = 640;
+        var hours = 8;
+        var companyEmployee = 640/8;
+        System.out.println("Всего работников в компании - "+ companyEmployee + " человек."  );
+        companyEmployee = companyEmployee + 94;
+        hours = totalHours/companyEmployee;
+        System.out.println("Если в компании работает " + companyEmployee + " человек, то всего " + hours +
+                " часов работы может быть поделено между сотрудниками ");
+
+
+    }
+}
